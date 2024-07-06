0d1ea196d8f41e503b0698cf9b6e7d9245cc5bf1
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-06 22:15:18 +0700

    add DashSideBar

---
+++ client/src/components/DashSideBar.jsx
@@ -0,0 +1,41 @@
+import { Sidebar } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+
+export default function DashSideBar() {
+  const [tab, setTab] = useState("");
+  const location = useLocation();
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-72">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item
+            active={tab === "profile"}
+            icon={HiUser}
+            label={"User"}
+            labelColor="dark"
+            href="/dashboard?tab=profile"
+          >
+            Profile
+          </Sidebar.Item>
+
+          <Sidebar.Item
+            // active
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
