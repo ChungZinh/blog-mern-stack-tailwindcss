ec8dfccf3bb434d9e439a76ab88f880873b05b94
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-09 22:17:23 +0700

    add delete user client

--- client/src/api/user.api.js
+++ client/src/api/user.api.js
@@ -3,6 +3,9 @@ import {
   updateFailure,
   updateStart,
   updateSuccess,
+  deleteFailure,
+  deleteStart,
+  deleteSuccess,
 } from "../redux/user/userSlice";
 
 export const updateUser = async (dispatch, user, formData) => {
@@ -27,5 +30,30 @@ export const updateUser = async (dispatch, user, formData) => {
   } catch (err) {
     console.error(err);
     dispatch(updateFailure());
+  }
+};
+
+export const deleteUser = async (dispatch, user) => {
+  dispatch(deleteStart());
+  try {
+    const res = await fetch(`${serverUrl}/api/user/delete/${user._id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        authorization: localStorage.getItem("token"),
+        "x-client-id": user._id,
+      },
+    });
+    // remove token from local storage
+    localStorage.removeItem("token");
+    const data = await res.json();
+    if (!res.ok) {
+      throw new Error(data.message || "Failed to delete user");
+    }
+    dispatch(deleteSuccess());
+    return data ? true : false;
+  } catch (error) {
+    console.error(error);
+    dispatch(deleteFailure());
   }
 };
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import {
   getDownloadURL,
@@ -7,10 +7,11 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import { updateUser } from "../api/user.api";
+import { deleteUser, updateUser } from "../api/user.api";
 import { toast } from "react-toastify";
 
 export default function DashProfile() {
@@ -23,6 +24,7 @@ export default function DashProfile() {
   const [imageFileUploadingError, setImageFileUploadingError] = useState(null);
   console.log(imageFileUploadingProgress, imageFileUploadingError);
   const [formData, setFormData] = useState({});
+  const [showModal, setShowModal] = useState(false);
   const filePickerRef = useRef();
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -74,18 +76,29 @@ export default function DashProfile() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     if (Object.keys(formData).length === 0) {
       toast.error("Form data is empty");
       return;
     }
-  
+
     try {
       const res = await updateUser(dispatch, currentUser, formData);
       console.log("res: ", res);
     } catch (err) {
       console.error("Error updating user: ", err);
       toast.error("Unexpected error occurred");
+    }
+  };
+
+  const handleDelete = async () => {
+    // delete user
+    const res = await deleteUser(dispatch, currentUser);
+    if (res) {
+      toast.success("Account deleted successfully");
+      setShowModal(false);
+    } else {
+      toast.error("Failed to delete account");
     }
   };
 
@@ -169,9 +182,33 @@ export default function DashProfile() {
         </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
-        <span className="cursor-pointer"> Delete Account</span>
+        <span onClick={() => setShowModal(true)} className="cursor-pointer">
+          Delete Account
+        </span>
         <span className="cursor-pointer"> Sign Out</span>
       </div>
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size="md"
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <HiOutlineExclamationCircle className="text-5xl text-red-500 mx-auto" />
+          <p className="text-center text-gray-500 mt-4">
+            Are you sure you want to delete your account?
+          </p>
+          <div className="flex justify-between mt-5">
+            <Button color="failure" onClick={handleDelete}>
+              Yes, I'm sure
+            </Button>
+            <Button color="gray" onClick={() => setShowModal(false)}>
+              Cancel
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
