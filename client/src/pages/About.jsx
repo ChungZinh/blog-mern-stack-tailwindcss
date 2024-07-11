99511052d82c8992d6b2f258f2d3f094f6e5ee73
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-11 19:38:32 +0700

    client: add aboutpage, projectspage

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,34 @@
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Dev Blog
+          </h1>
+          <div className="text-base text-neutral-500 flex flex-col gap-6">
+            <p>
+              Dev Blog is a platform for developers to share their knowledge and
+              expertise with the community. We believe that sharing knowledge
+              helps the community grow and learn from each other. We encourage
+              all developers to share their knowledge and help others learn.
+            </p>
+            <p>
+              Dev Blog is a platform for developers to share their knowledge and
+              expertise with the community. We believe that sharing knowledge
+              helps the community grow and learn from each other. We encourage
+              all developers to share their knowledge and help others learn.
+            </p>
+
+            <p>
+              Dev Blog is a platform for developers to share their knowledge and
+              expertise with the community. We believe that sharing knowledge
+              helps the community grow and learn from each other. We encourage
+              all developers to share their knowledge and help others learn.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div
+      className="min-h-screen max-w-2xl
+     mx-auto flex justify-center items-center flex-col gap-6 p-3"
+    >
+      <h1 className="text-3xl font-semibold ">Projects</h1>
+      <p className="text-base text-neutral-600">
+        Build a portfolio of projects that you can show to potential employers.
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
