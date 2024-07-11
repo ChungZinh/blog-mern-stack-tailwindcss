9c3c41470edc07ebfb17ba5a28de85b0671afb4f
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-11 16:35:01 +0700

    clientr: add homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { serverUrl } from "../constants";
+import PostCard from "../components/PostCard";
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      if (currentUser && currentUser.isAdmin) {
+        try {
+          const res = await fetch(`${serverUrl}/api/post/getposts?limit=5`, {
+            method: "GET",
+            headers: {
+              "Content-type": "application/json",
+              authorization: localStorage.getItem("token"),
+              "x-client-id": currentUser._id,
+            },
+          });
+          const data = await res.json();
+          if (res.ok) {
+            setPosts(data.data.posts);
+            console.log("posts:", posts);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    fetchPosts();
+  }, [currentUser]);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <p className="text-gray-500 lg:text-lg md:text-base sm:text-sm">
+          Here you'll find a variety of articles and tutorials on web
+          development, programming, and more.
+        </p>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm
+      text-teal-500 font-bold hover:underline
+      "
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
