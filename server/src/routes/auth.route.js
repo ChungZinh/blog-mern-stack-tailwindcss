6c76009f2c2e34a5a4995918c100ac83656e013e
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-11 22:52:08 +0700

    update route test

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const authController = require("../controllers/auth.controller");
 const { asyncHandler } = require("../helpers/asyncHandler");
 
+router.get("/", (req, res) => {
+  res.send("ROUTE AUTH");
+});
 router.post("/signup", asyncHandler(authController.signUp));
 router.post("/signin", asyncHandler(authController.signIn));
-router.post('/google', asyncHandler(authController.googleAuth))
+router.post("/google", asyncHandler(authController.googleAuth));
 router.use(require("../auth/authUtils").verifyToken);
-router.post('/logout', asyncHandler(authController.logout))
+router.post("/logout", asyncHandler(authController.logout));
 module.exports = router;
