f81f905025ed51d1fbd13d4832ed21e01d3f745d
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-11 23:50:41 +0700

    update sidebar

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -31,46 +31,63 @@ export default function DashSideBar() {
   return (
     <Sidebar className="w-full md:w-72">
       <Sidebar.Items>
-        <Sidebar.ItemGroup className="gap-4">
-          {currentUser.isAdmin && (
-            <Sidebar.Item icon={HiChartPie} labelColor="dark">
-              <Link to={"/dashboard?tab=dashboard"}>Dashboard</Link>
+        <Sidebar.ItemGroup>
+            {currentUser.isAdmin && (
+              <Sidebar.Item
+                active={tab === "dashboard"}
+                icon={HiChartPie}
+                labelColor="dark"
+              >
+                <Link to={"/dashboard?tab=dashboard"}>Dashboard</Link>
+              </Sidebar.Item>
+            )}
+            <Sidebar.Item
+              icon={HiUser}
+              label={currentUser.isAdmin ? "Admin" : "User"}
+              labelColor="dark"
+              active={tab === "profile"}
+            >
+              <Link to={"/dashboard?tab=profile"}>Profile</Link>
             </Sidebar.Item>
-          )}
-          <Sidebar.Item
-            icon={HiUser}
-            label={currentUser.isAdmin ? "Admin" : "User"}
-            labelColor="dark"
-          >
-            <Link to={"/dashboard?tab=profile"}>Profile</Link>
-          </Sidebar.Item>
 
-          {currentUser.isAdmin && (
-            <Sidebar.Item icon={HiDocumentText} labelColor="dark">
-              <Link to={"/dashboard?tab=posts"}>Posts</Link>
+            {currentUser.isAdmin && (
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiDocumentText}
+                labelColor="dark"
+              >
+                <Link to={"/dashboard?tab=posts"}>Posts</Link>
+              </Sidebar.Item>
+            )}
+
+            {currentUser.isAdmin && (
+              <Sidebar.Item
+                active={tab === "users"}
+                icon={HiUserGroup}
+                labelColor="dark"
+              >
+                <Link to={"/dashboard?tab=users"}>Users</Link>
+              </Sidebar.Item>
+            )}
+
+            {currentUser.isAdmin && (
+              <Sidebar.Item
+                active={tab === "comments"}
+                icon={HiAnnotation}
+                labelColor="dark"
+              >
+                <Link to={"/dashboard?tab=comments"}>Comments</Link>
+              </Sidebar.Item>
+            )}
+
+            <Sidebar.Item
+              // active
+              icon={HiArrowSmRight}
+              className="cursor-pointer"
+              onClick={handleLogout}
+            >
+              Sign Out
             </Sidebar.Item>
-          )}
-
-          {currentUser.isAdmin && (
-            <Sidebar.Item icon={HiUserGroup} labelColor="dark">
-              <Link to={"/dashboard?tab=users"}>Users</Link>
-            </Sidebar.Item>
-          )}
-
-          {currentUser.isAdmin && (
-            <Sidebar.Item icon={HiAnnotation} labelColor="dark">
-              <Link to={"/dashboard?tab=comments"}>Comments</Link>
-            </Sidebar.Item>
-          )}
-
-          <Sidebar.Item
-            // active
-            icon={HiArrowSmRight}
-            className="cursor-pointer"
-            onClick={handleLogout}
-          >
-            Sign Out
-          </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
